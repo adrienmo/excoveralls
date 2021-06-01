8f131e93935b1281d516d2406b855cf8ebaec76f
Adrien Moreau <adrien@nash.io>
2021-06-01 11:28:49 +0800

    filter app name
    Signed-off-by: Adrien Moreau <adrien@nash.io>

--- lib/excoveralls.ex
+++ lib/excoveralls.ex
@@ -39,6 +39,7 @@ defmodule ExCoveralls do
       base_compile_path = Path.expand(compile_path <> "../../..")
       Mix.Dep.cached() 
       |> Enum.filter(& is_nil(&1.opts[:lock])) 
+      |> Enum.reject(&(&1.app in [:nex_protocol]))
       |> Enum.map(& "#{base_compile_path}/#{&1.app}/ebin")
       |> Kernel.++([compile_path])
     else
